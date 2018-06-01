0fb1240ea1d881e0d85ecb359d33c06d64ee2c23
Pamela Frieze <pfrieze@gmail.com>
2018-06-01 23:06:30 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,11 +11,10 @@ class Student
   end
 
   def self.all
-    sql = <<-SQLite3
+    sql = <<-SQL
     SELECT *
     FROM songs
     SQL
-  end
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
   end
