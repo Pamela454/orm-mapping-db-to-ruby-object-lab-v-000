6e202fbc35ac264ec75303b9f3056c291984c132
Pamela Frieze <pfrieze@gmail.com>
2018-06-01 23:37:34 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -116,5 +116,16 @@ class Student
   end
 
   def self.all_students_in_grade_X(grade)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = (grade)
+      ORDER BY students.id
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end.first
   end
 end
