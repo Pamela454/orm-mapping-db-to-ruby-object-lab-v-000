09a390979403aff22d5effc200678a1e0cc725f6
Pamela Frieze <pfrieze@gmail.com>
2018-06-01 23:17:05 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -64,7 +64,7 @@ class Student
     DB[:conn].execute(sql)
   end
 
-  def count_all_students_in_grade_9
+  def self.count_all_students_in_grade_9
     sql = <<-SQL
       SELECT *
       FROM students
