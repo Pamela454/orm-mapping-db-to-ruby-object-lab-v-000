a59f691798a6a4af8d3da13124c9137728b73b1d
Pamela Frieze <pfrieze@gmail.com>
2018-06-01 23:18:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -76,5 +76,12 @@ class Student
   end
 
   def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 9
+    SQL
+
+    DB[:conn].execute(sql)
   end
 end
