ae6eab96031f67a58e6df5917692b43fd1d497da
Pamela Frieze <pfrieze@gmail.com>
2018-06-01 23:22:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,6 +82,7 @@ class Student
       WHERE grade < 12
     SQL
 
-    DB[:conn].execute(sql, self.name, self.grade)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
   end
 end
