1412e7af12800367e523496663ca8f5801d4f780
Pamela Frieze <pfrieze@gmail.com>
2018-06-01 23:25:02 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -88,5 +88,11 @@ class Student
   end
 
   def self.first_X_students_in_grade_10(number)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12
+      LIMIT (number)
+    SQL
   end
 end
